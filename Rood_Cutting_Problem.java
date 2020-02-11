07356126c04c7ea312ffb5063bc2b697a5dce03e
Admin <naveenkaushal441@gmail.com>
2020-02-11 23:35:33 +0530

    Road Cutting Problem  solution added

---
+++ Rood_Cutting_Problem.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.util.Scanner;  // but not required due to line no. 1
+
+class Naveen
+{
+	public static void main(String args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int arr[] = new int[n];
+
+		for(int i=0;i<n;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.println(Naveen.Find(arr,n));
+	}
+
+	static int Find(int price[], int n)
+	{
+
+    int dp[][] = new int [n+1][n+1];
+
+
+    for(int i=0;i<=n;i++)
+    {
+    	for(int j=0;j<=n;j++)
+    	{
+
+           if(i == 0 || j == 0)
+           {
+           	dp[i][j] = 0;
+           }
+           else
+           {
+           	if(i>j)
+           	{
+           		dp[i][j] = dp[i-1][j];
+           	}
+           	else
+           	{
+           		dp[i][j] = Math.max(price[i-1]+dp[i][j-1],dp[i-1][j]);
+           	}
+           }
+
+    	}
+    }
+
+    return (dp[n][n]);
+
+	}
+}
